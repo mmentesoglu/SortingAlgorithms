92582e4b10e806a0a4af694ff24df1fbedc654c9
mmentesoglu <mmentesoglu@gmail.com>
2016-07-04 12:17:26 +0300

    function parameters to pointer

--- bubble001/Bubble/main.c
+++ bubble001/Bubble/main.c
@@ -3,50 +3,50 @@
     This application created for demonstrate of bubble sort
     Author : MMENTESOGLU
     Time   : 2016-07-04
-    Version: 0.0.2
-    - Added Compare function
+    Version: 0.0.3
+    - Change function parameter to pointer
 */
 #include <stdio.h>
 #include <stdlib.h>
-#define N_Array 255
-#define DEBUG 1
+#define N_Array 65536
+#define DEBUG 0
 
 void printArray(int *array,int len);
-void bubbleSort(int a[],int N);
-int compare(int a,int b);
+void bubbleSort(int *p,int N);
+int compare(int *a,int *b);
 
 int arr[10] = {3,6,1,2,3,8,4,1,7,2};
-int dizi[N_Array];
+int array[N_Array];
 
 int main()
 {
     int i=0;
-    for(i = N_Array-1;i>=0;i--)dizi[N_Array-i] = i;
+    for(i = N_Array-1;i>=0;i--)array[N_Array-i] = i;
 #if DEBUG
-    printArray(dizi,N_Array);
+    printArray(array,N_Array);
 #endif // DEBUG
-    bubbleSort(dizi,N_Array);
+    bubbleSort(array,N_Array);
 #if DEBUG
-    printArray(dizi,N_Array);
+    printArray(array,N_Array);
 #endif // DEBUG
     return 0;
 }
 
-void bubbleSort(int a[],int N){
+void bubbleSort(int *p,int N){
     int i,j,t;
     for(i = N-1;i>=0;i--){
         for(j=1;j<=i;j++){
-            if(compare(a[j-1],a[j])){
-                t = a[j-1];
-                a[j-1] = a[j];
-                a[j] = t;
+            if(compare(&p[j-1],&p[j])){
+                t = p[j-1];
+                p[j-1] = p[j];
+                p[j] = t;
             }
         }
     }
 }
 
-int compare(int a,int b){
-    return (a>b);
+int compare(int *a,int *b){
+    return (*a>*b);
 }
 
 void printArray(int *array,int len){
